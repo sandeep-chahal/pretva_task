8d3050238549194731f2d564c56c0dfea1071051
sandeep chahal <Sandeepchahal573@gmail.com>
2020-10-09 13:24:43 +0530

    added production support

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
@@ -9,7 +10,6 @@ const app = express();
 
 const main = async () => {
 	//middleware
-	app.use(express.static("./client/build"));
 	app.use(express.json());
 
 	// connect to db
@@ -20,6 +20,13 @@ const main = async () => {
 
 	// routes
 	app.use(productRoute);
+	if (process.env.NODE_ENV === "production") {
+		app.use(express.static("client/build"));
+
+		app.get("*", (req, res) => {
+			res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+		});
+	}
 
 	// catching errors
 	app.use((err, req, res, next) => {
